7f9495582e21a2f6213a8fcad96c413ecfcfe7a9
uyomere <uyomerekunu@gmail.com>
2026-03-09 09:35:00 +0100

    Second Changes Added Car Array

--- index.js
+++ index.js
@@ -54,5 +54,7 @@ setInterval(sayHello, 1000)
 
 const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
 
+const car = ['Benz', 'Toyota', 'Camry', 'Honda', 'Ford']
+
 
 countries.forEach((element) => {console.log(element.toUpperCase())})
